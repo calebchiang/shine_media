b5f8438e0b404030ef3588c51841285b41101794
Caleb Chiang <calebchiang1002@gmail.com>
2024-06-24 13:47:03 -0700

    Added request consulation button and changed nav bar styling.

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 function Hero() {
     return (
         <div className="bg-blue-50 p-36 font-roboto flex items-center justify-center pl-32">
@@ -8,6 +8,11 @@ function Hero() {
                 <p className="text-2xl mt-4">Empowering Your Company to <span className="text-blue-500 font-bold">Shine
                 </span> in Today's Digital Ecosystem.
                 </p>
+                <Link to="/contact">
+                    <button className="mt-4 py-2 px-6 bg-green-500 text-white font-bold rounded-lg hover:bg-green-700 transition duration-300">
+                        Request Consultation
+                    </button>
+                </Link>
             </div>
             <div className="flex-1 flex justify-center items-center">
                 <img src="/hvac_stock.jpeg" alt="Hero Image" className="w-4/5 h-auto rounded-lg" />
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -9,12 +9,12 @@ function Navbar() {
             </Link>
             <h1 className="text-2xl font-bold"><Link to="/">shine media</Link></h1>
             <div className="text-2xl ml-80">
-            <Link to="/services" className="mx-4 text-xl ml-64  hover:text-blue-500">Services</Link>
-            <Link to="/about" className="mx-4 text-xl ml-30 hover:text-blue-500">About Us</Link>
-            <Link to="/contact" className="mx-4 text-xl ml-30 hover:text-blue-500">Contact</Link>
+            <Link to="/services" className="mx-4 text-xl ml-64  hover:underline">Services</Link>
+            <Link to="/about" className="mx-4 text-xl ml-30 hover:underline">About Us</Link>
+            <Link to="/contact" className="mx-4 text-xl ml-30 hover:underline">Contact</Link>
             </div>
             <Link to="/contact">
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full ml-4">
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md ml-4">
                     Hire Us Today
                 </button>
             </Link>
